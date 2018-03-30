fcb204267cbe2234b74f7cc51009fcd1c56db852
lzzy12 <jhashivam2020@gmail.com>
2018-03-30 05:07:48 +0000

    exynos7870: Update sdcardfs with oreo changes
    We have on7 prime kernel which is oreo ready from the OEM (Samsung), hence we can port this from it
    
    Signed-off-by: lzzy12 <jhashivam2020@gmail.com>

--- fs/sdcardfs/m/dentry.c
+++ fs/sdcardfs/m/dentry.c
@@ -34,8 +34,9 @@ static int sdcardfs_d_revalidate(struct dentry *dentry, unsigned int flags)
 	struct dentry *parent_lower_dentry = NULL;
 	struct dentry *lower_cur_parent_dentry = NULL;
 	struct dentry *lower_dentry = NULL;
-	struct inode *inode;
-	struct sdcardfs_inode_data *data;
+#if ANDROID_VERSION >= 70000
+	struct sdcardfs_inode_info *pinfo;
+#endif
 
 	if (flags & LOOKUP_RCU)
 		return -ECHILD;
@@ -45,11 +46,16 @@ static int sdcardfs_d_revalidate(struct dentry *dentry, unsigned int flags)
 		spin_unlock(&dentry->d_lock);
 		return 1;
 	}
+	if (dentry->d_flags & DCACHE_WILL_INVALIDATE) {
+		dentry->d_flags &= ~DCACHE_WILL_INVALIDATE;
+		__d_drop(dentry);
+		spin_unlock(&dentry->d_lock);
+		return 0;
+	}
 	spin_unlock(&dentry->d_lock);
 
 	/* check uninitialized obb_dentry and
-	 * whether the base obbpath has been changed or not
-	 */
+	 * whether the base obbpath has been changed or not */
 	if (is_obbpath_invalid(dentry)) {
 		d_drop(dentry);
 		return 0;
@@ -62,13 +68,15 @@ static int sdcardfs_d_revalidate(struct dentry *dentry, unsigned int flags)
 	lower_dentry = lower_path.dentry;
 	lower_cur_parent_dentry = dget_parent(lower_dentry);
 
-	if ((lower_dentry->d_flags & DCACHE_OP_REVALIDATE)) {
-		err = lower_dentry->d_op->d_revalidate(lower_dentry, flags);
-		if (err == 0) {
-			d_drop(dentry);
-			goto out;
-		}
+#if ANDROID_VERSION >= 70000
+	pinfo = SDCARDFS_I(parent_dentry->d_inode);
+	if (pinfo->perm == PERM_ANDROID_OBB && dentry->d_inode &&
+			uid_eq(dentry->d_inode->i_uid, GLOBAL_ROOT_UID)) {
+		d_drop(dentry);
+		err = 0;
+		goto out;
 	}
+#endif
 
 	spin_lock(&lower_dentry->d_lock);
 	if (d_unhashed(lower_dentry)) {
@@ -85,15 +93,25 @@ static int sdcardfs_d_revalidate(struct dentry *dentry, unsigned int flags)
 		goto out;
 	}
 
-	if (dentry < lower_dentry) {
-		spin_lock(&dentry->d_lock);
-		spin_lock_nested(&lower_dentry->d_lock, DENTRY_D_LOCK_NESTED);
-	} else {
-		spin_lock(&lower_dentry->d_lock);
-		spin_lock_nested(&dentry->d_lock, DENTRY_D_LOCK_NESTED);
+	if (dentry == lower_dentry) {
+		err = 0;
+		panic("sdcardfs: dentry is equal to lower_dentry\n");
+		goto out;
 	}
 
-	if (!qstr_case_eq(&dentry->d_name, &lower_dentry->d_name)) {
+	if (dentry < lower_dentry) {
+		spin_lock(&dentry->d_lock);
+		spin_lock(&lower_dentry->d_lock);
+	} else {
+		spin_lock(&lower_dentry->d_lock);
+		spin_lock(&dentry->d_lock);
+	}
+
+	if (dentry->d_name.len != lower_dentry->d_name.len) {
+		__d_drop(dentry);
+		err = 0;
+	} else if (strncasecmp(dentry->d_name.name, lower_dentry->d_name.name,
+				dentry->d_name.len) != 0) {
 		__d_drop(dentry);
 		err = 0;
 	}
@@ -104,21 +122,6 @@ static int sdcardfs_d_revalidate(struct dentry *dentry, unsigned int flags)
 	} else {
 		spin_unlock(&dentry->d_lock);
 		spin_unlock(&lower_dentry->d_lock);
-	}
-	if (!err)
-		goto out;
-
-	/* If our top's inode is gone, we may be out of date */
-	inode = igrab(dentry->d_inode);
-	if (inode) {
-		data = top_data_get(SDCARDFS_I(inode));
-		if (!data || data->abandoned) {
-			d_drop(dentry);
-			err = 0;
-		}
-		if (data)
-			data_put(data);
-		iput(inode);
 	}
 
 out:
@@ -132,10 +135,12 @@ out:
 static void sdcardfs_d_release(struct dentry *dentry)
 {
 	/* release and reset the lower paths */
-	if (has_graft_path(dentry))
+	if (has_graft_path(dentry)) {
 		sdcardfs_put_reset_orig_path(dentry);
+	}
 	sdcardfs_put_reset_lower_path(dentry);
 	free_dentry_private_data(dentry);
+	return;
 }
 
 static int sdcardfs_hash_ci(const struct dentry *dentry,
@@ -152,10 +157,12 @@ static int sdcardfs_hash_ci(const struct dentry *dentry,
 	unsigned long hash;
 
 	name = qstr->name;
+	/* len = vfat_striptail_len(qstr); */
 	len = qstr->len;
 
 	hash = init_name_hash();
 	while (len--)
+		/* hash = partial_name_hash(nls_tolower(t, *name++), hash); */
 		hash = partial_name_hash(tolower(*name++), hash);
 	qstr->hash = end_name_hash(hash);
 
@@ -166,20 +173,31 @@ static int sdcardfs_hash_ci(const struct dentry *dentry,
  * Case insensitive compare of two vfat names.
  */
 static int sdcardfs_cmp_ci(const struct dentry *parent,
-		const struct dentry *dentry,
-		unsigned int len, const char *str, const struct qstr *name)
+		const struct dentry *dentry, unsigned int len,
+		const char *str, const struct qstr *name)
 {
+	/* This function is copy of vfat_cmpi */
 	/* FIXME Should we support national language? */
+	/* struct nls_table *t = MSDOS_SB(parent->d_sb)->nls_io; */
+	/* unsigned int alen, blen; */
 
+	/* A filename cannot end in '.' or we treat it like it has none */
+	/*
+	alen = vfat_striptail_len(name);
+	blen = __vfat_striptail_len(len, str);
+	if (alen == blen) {
+		if (nls_strnicmp(t, name->name, str, alen) == 0)
+			return 0;
+	}
+	*/
 	if (name->len == len) {
-		if (str_n_case_eq(name->name, str, len))
+		if (strncasecmp(name->name, str, len) == 0)
 			return 0;
 	}
 	return 1;
 }
 
-static void sdcardfs_canonical_path(const struct path *path,
-				struct path *actual_path)
+static void sdcardfs_canonical_path(const struct path *path, struct path *actual_path)
 {
 	sdcardfs_get_real_lower(path->dentry, actual_path);
 }
@@ -187,7 +205,7 @@ static void sdcardfs_canonical_path(const struct path *path,
 const struct dentry_operations sdcardfs_ci_dops = {
 	.d_revalidate	= sdcardfs_d_revalidate,
 	.d_release	= sdcardfs_d_release,
-	.d_hash	= sdcardfs_hash_ci,
+	.d_hash		= sdcardfs_hash_ci,
 	.d_compare	= sdcardfs_cmp_ci,
 	.d_canonical_path = sdcardfs_canonical_path,
 };
--- fs/sdcardfs/o/super.c
+++ fs/sdcardfs/o/super.c
@@ -78,6 +78,12 @@ static int sdcardfs_statfs(struct dentry *dentry, struct kstatfs *buf)
 	err = vfs_statfs(&lower_path, buf);
 	sdcardfs_put_lower_path(dentry, &lower_path);
 
+	if (uid_eq(GLOBAL_ROOT_UID, current_fsuid()) ||
+			capable(CAP_SYS_RESOURCE) ||
+			in_group_p(AID_USE_ROOT_RESERVED) ||
+			in_group_p(AID_USE_SEC_RESERVED))
+		goto out;
+
 	if (sbi->options.reserved_mb) {
 		/* Invalid statfs informations. */
 		if (buf->f_bsize == 0) {
@@ -96,7 +102,7 @@ static int sdcardfs_statfs(struct dentry *dentry, struct kstatfs *buf)
 		/* Make reserved blocks invisiable to media storage */
 		buf->f_bfree = buf->f_bavail;
 	}
-
+out:
 	/* set return buf to our f/s to avoid confusing user-level utils */
 	buf->f_type = SDCARDFS_SUPER_MAGIC;
 
